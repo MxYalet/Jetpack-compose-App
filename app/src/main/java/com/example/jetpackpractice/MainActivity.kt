8e8a85c379dbf0f6359412cb69499e440d821865
mac <muhammedowoyale@gmail.com>
2024-01-13 00:08:38 +0100

    made button responsive

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +25,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
            myApp()
         }
     }
@@ -34,28 +35,32 @@ data class Message(val author: String, val message: String)
 @Composable
 fun myApp(names: List<String> = listOf("world", "compose")){
  Surface( modifier = Modifier.padding(vertical = 4.dp)){
-         for (name in names){
+     Column {
+         for (name in names) {
              Message(name)
          }
+     }
      }
  }
 
 
 @Composable
 fun Message(name: String) {
-    Surface(color = Color.Red,
+    val extended = remember{ mutableStateOf(false) }
+    val extraPadding = if (extended.value) 48.dp else 0.dp
+    Surface( color = Color.Magenta,
         modifier = Modifier.padding( horizontal = 8.dp, vertical = 4.dp )) {
-        Row{
+        Row(modifier = Modifier.padding(24.dp)){
             Column(
-                modifier = Modifier
-                    .padding(24.dp)
-                    .weight(1f)
+                modifier = Modifier.weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
-                Text(text = name)
-            }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+                Text(text = name)}
+            OutlinedButton(onClick = {
+                extended.value = !extended.value
+            }) {
+                Text(if (extended.value) "Show Less" else "Show More")
             }
         }
     }
