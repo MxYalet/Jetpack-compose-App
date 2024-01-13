d7d121480f6587219cef879e03d6361b97f1f375
mac <muhammedowoyale@gmail.com>
2024-01-13 01:05:14 +0100

    handling states

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -9,11 +9,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackpractice.ui.theme.JetpackPracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,25 +49,28 @@ fun myApp(){
     }
     else{ Greeting()
     }
-
 }
 
 @Composable
-fun Greeting(names: List<String> = listOf("world", "compose")){
- Surface( modifier = Modifier.padding(vertical = 4.dp)){
-     Column {
-         for (name in names) {
-             Message(name)
-         }
-     }
-     }
- }
-
+fun Greeting(names: List<String> = List(1000){"$it"}){
+    Surface( modifier = Modifier.padding(vertical = 4.dp)){
+        Column {
+            /* for (name in names) {
+                 Message(name)
+             }*/
+            LazyColumn{
+                items(names){ name ->
+                    Message(name)
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun Message(name: String) {
-    val extended = remember{ mutableStateOf(false) }
-    val extraPadding = if (extended.value) 48.dp else 0.dp
+    var extended  by remember{ mutableStateOf(false) }
+    val extraPadding = if (extended) 48.dp else 0.dp
     Surface( color = Color.Magenta,
         modifier = Modifier.padding( horizontal = 8.dp, vertical = 4.dp )) {
         Row(modifier = Modifier.padding(24.dp)){
@@ -81,13 +82,14 @@ fun Message(name: String) {
                 Text(text = "Hello")
                 Text(text = name)}
             OutlinedButton(onClick = {
-                extended.value = !extended.value
+                extended = !extended
             }) {
-                Text(if (extended.value) "Show Less" else "Show More")
+                Text(if (extended) "Show Less" else "Show More")
             }
         }
     }
 }
+
 @Composable
 fun OnboardingScreen(onContinueClicked: ()-> Unit) {
     Column(
@@ -105,7 +107,6 @@ fun OnboardingScreen(onContinueClicked: ()-> Unit) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
