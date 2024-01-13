59fd736e71a47b047141752f2820f4ac0bbd5a66
mac <muhammedowoyale@gmail.com>
2024-01-13 01:05:14 +0100

    handling states

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.jetpackpractice
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,8 +40,22 @@ class MainActivity : ComponentActivity() {
 
 data class Message(val author: String, val message: String)
 
+
 @Composable
-fun myApp(names: List<String> = listOf("world", "compose")){
+fun myApp(){
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }
+    else{ Greeting()
+    }
+
+}
+
+@Composable
+fun Greeting(names: List<String> = listOf("world", "compose")){
  Surface( modifier = Modifier.padding(vertical = 4.dp)){
      Column {
          for (name in names) {
@@ -52,7 +74,8 @@ fun Message(name: String) {
         modifier = Modifier.padding( horizontal = 8.dp, vertical = 4.dp )) {
         Row(modifier = Modifier.padding(24.dp)){
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
                     .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
@@ -65,9 +88,32 @@ fun Message(name: String) {
         }
     }
 }
+@Composable
+fun OnboardingScreen(onContinueClicked: ()-> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+        OnboardingScreen(onContinueClicked = {})
+}
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun MessagePreview(){
-    myApp()
+    Greeting()
 }
