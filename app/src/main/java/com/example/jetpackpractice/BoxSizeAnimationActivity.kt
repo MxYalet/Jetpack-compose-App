da945f8c2cbebea41c8bb88da33a35f224b500e0
mac <muhammedowoyale@gmail.com>
2024-02-28 00:40:19 +0100

    Animation practice

---
+++ app/src/main/java/com/example/jetpackpractice/BoxSizeAnimationActivity.kt
@@ -0,0 +1,72 @@
+package com.example.jetpackpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.InfiniteTransition
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackpractice.ui.theme.JetpackPracticeTheme
+
+class BoxSizeAnimationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sizeState by remember { mutableStateOf(200.dp) }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+                tween(
+                    durationMillis = 4000,
+                    delayMillis = 300,
+                    easing = LinearOutSlowInEasing
+                ),
+             //can also use spring instead of tween and keyframs also
+                label = ""
+            )
+
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition. animateColor(
+                initialValue = Color.Black,
+                targetValue = Color.Red,
+                animationSpec = infiniteRepeatable(
+                    tween(2000),
+                    repeatMode = RepeatMode.Reverse
+                ), label = ""
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = { sizeState += 50.dp}) {
+                    Text(text = "Increase Size")
+                }
+            }
+        }
+    }
+}
+
