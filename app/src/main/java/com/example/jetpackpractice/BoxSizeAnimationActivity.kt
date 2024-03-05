91bac52549f433a7148640d562442df8d394ecf1
mac <muhammedowoyale@gmail.com>
2024-03-05 23:36:06 +0100

    Animation practice

--- app/src/main/java/com/example/jetpackpractice/BoxSizeAnimationActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/BoxSizeAnimationActivity.kt
@@ -58,15 +58,7 @@ class BoxSizeAnimationActivity : ComponentActivity() {
                     repeatMode = RepeatMode.Reverse
                 ), label = ""
             )
-            Box(
-                modifier = Modifier
-                    .size(size)
-                    .background(color),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(onClick = { sizeState += 50.dp}) {
-                    Text(text = "Increase Size")
-                }
+
             }
         }
     }
